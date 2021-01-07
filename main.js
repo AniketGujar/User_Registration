f0598014aac873d7fb344d8e7baaa59f6ab78037
AniketGujar <gujaraniket7875@gmail.com>
2021-01-08 00:28:49 +0530

    Added: Last Name Validation

--- main.js
+++ main.js
@@ -3,13 +3,23 @@ let fName = "";
 
 firstName = () => {
     let firstNamePattern = /^[A-Z]+[a-zA-Z]{2,}$/;
-    fName = readlineSync.question('Enter your name: ');
+    fName = readlineSync.question('Enter your First Name: ');
     const result = fName.match(firstNamePattern);
     if (result == null) {
         console.log("Please Enter Valid First Name(Starts with capital, Min 3 char)");
         firstName();
     }
 }
+lastName = () => {
+    let lastNamePattern = /^[A-Z]+[a-zA-Z]{2,}$/;
+    let lName = readlineSync.question('Enter your Last Name: ');
+    const result = lName.match(lastNamePattern);
+    if (result == null) {
+        console.log("Please Enter Valid Last Name(Starts with capital, Min 3 char)");
+        lastName();
+    }
+}
 
 firstName();
 console.log("HI " + fName + " !");
+lastName();

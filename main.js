03ab95e0f7e946d5356875797926f4691f1335f4
AniketGujar <gujaraniket7875@gmail.com>
2021-01-08 00:42:40 +0530

    Added: Email Validation Code

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 const readlineSync = require('readline-sync');
-let fName = "";
+let first_Name = "";
 
+//User First Name Validation
 firstName = () => {
     let firstNamePattern = /^[A-Z]+[a-zA-Z]{2,}$/;
-    fName = readlineSync.question('Enter your First Name: ');
-    const result = fName.match(firstNamePattern);
+    first_Name = readlineSync.question('Enter your First Name: ');
+    const result = first_Name.match(firstNamePattern);
     if (result == null) {
         console.log("Please Enter Valid First Name(Starts with capital, Min 3 char)");
         firstName();
     }
 }
+
+//User Last Name Validation
 lastName = () => {
     let lastNamePattern = /^[A-Z]+[a-zA-Z]{2,}$/;
-    let lName = readlineSync.question('Enter your Last Name: ');
-    const result = lName.match(lastNamePattern);
+    let last_Name = readlineSync.question('Enter your Last Name: ');
+    const result = last_Name.match(lastNamePattern);
     if (result == null) {
         console.log("Please Enter Valid Last Name(Starts with capital, Min 3 char)");
         lastName();
     }
 }
 
+//User Email Validation
+userEmail = () => {
+    let emailPattern = /^([a-z]+[0-9a-z-!#$%+^&*_.]*){3,15}@[a-z0-9]{1,8}[.]*([a-z]{2,4})*.[a-z]{2,4}/;
+    let email = readlineSync.question('Enter your Email-Id: ');
+    const result = email.match(emailPattern);
+    if (result == null) {
+        console.log("Please Enter Valid Email ");
+        userEmail();
+    }
+}
+
 firstName();
-console.log("HI " + fName + " !");
+console.log("HI " + first_Name + " !");
 lastName();
+userEmail();
